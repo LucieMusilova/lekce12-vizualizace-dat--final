96b6658260051f6c12fac7063d072eb28918a115
karolyim <matej.karolyi@gmail.com>
2021-05-30 21:04:41 +0200

    [FEATURE] additional hospitalization data added tothe graph after timeout

--- covid19-pocty-hospitalizovanych.js
+++ covid19-pocty-hospitalizovanych.js
@@ -6,9 +6,16 @@ fetch('https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/hospitalizace.min.jso
           ['Celkový počet hospitlizovaných']
         ];
 
+        dataHospDalsi = [
+          ['Stav bez příznaků nebo lehký'],
+          ['Stav střední nebo těžký']
+        ];
+
         data.data.forEach(zaznam => {
           dataHosp[0].push(zaznam.datum);
           dataHosp[1].push(zaznam.pocet_hosp);
+          dataHospDalsi[0].push(zaznam.stav_bez_priznaku + zaznam.stav_lehky);
+          dataHospDalsi[1].push(zaznam.stav_stredni + zaznam.stav_tezky);
         });
 
         var graf = c3.generate({
@@ -26,6 +33,12 @@ fetch('https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/hospitalizace.min.jso
             }
         }
       });
+
+      setTimeout(function () {
+        graf.load({
+            columns: dataHospDalsi
+        });
+    }, 2000);
     })
     .catch((error) => {
         console.error('Chyba aplikace: ', error);
